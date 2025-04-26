6c1da69937bf8c97861596dd3aef67a23750545a
Carlos <cbiosca82@gmail.com>
2025-04-26 17:01:18 +0200

    socket connection

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
-const io = require('socket.io')(server);
-
 const port = process.env.PORT || 8000;
+
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.emit('message', 'Welcome to the server!');
 });
 
 server.listen(port, () => {
